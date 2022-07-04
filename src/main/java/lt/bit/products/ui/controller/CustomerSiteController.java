305798334f1023080066a30f5d17f6624deec6b6
SinusaiteJustina <97383036+SinusaiteJustina@users.noreply.github.com>
2022-07-04 22:03:06 +0300

    Consumer Site: products

---
+++ src/main/java/lt/bit/products/ui/controller/CustomerSiteController.java
@@ -0,0 +1,18 @@
+package lt.bit.products.ui.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class CustomerSiteController {
+
+    @PostMapping("/cart/add")
+    @ResponseBody
+    void addToCart(@RequestParam("productId") String productId) {
+        System.out.println(productId);
+        }
+    }
+
